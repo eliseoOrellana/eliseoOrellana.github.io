f6fd76adab3802aa06c75345632066206e8e7061
Eliseo Orellana <97992106+eliseoOrellana@users.noreply.github.com>
2024-01-19 01:46:39 -0300

    first
    feat(projectProtafolio): first commit

---
+++ port2024/dfdf.js
@@ -0,0 +1,27 @@
+
+
+class AboutMe {
+    constructor() {
+        this.code = ['Java', 'Python', 'Javascript', 'HTML', 'CSS', 'SQL', 'NoSQL'];
+        this.tecnologias = {
+            Java: ['SpringBoot', 'Spring Security', 'Microservicios', 'Hibernate', 'Thymeleaf', 'JPA', 'JDBC'],
+            Javascript: ['React', 'jQuery', 'Ajax', 'DataTables'],
+            Python: ['Django', 'FastApi'],
+            Css: ['Bootstrap'],
+            Databases: ['MySQL', 'Oracle', 'SQLite', 'MongoDB'],
+            Mobile: ['Android'],
+            NodeJS: true,
+            NestJS: true,
+            Typescript: true,
+            Angular: true,
+            VersionControl: ['Git', 'GitHub'],
+            Docker: ['Dockerfile', 'Docker Compose'],
+        };
+        this.freeTime = ['FamilyTime', 'Chess'];
+        this.currentOccupation = ['Analyst Programmer | Full Stack Java Developer, open to job opportunities...']; 
+    }
+}
+
+
+
+
